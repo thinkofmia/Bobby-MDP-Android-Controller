169cc891d74dfe38ada01ac0b1bb4382b915ce7e
Alexander Wong <alexanderWCX@gmail.com>
2020-09-30 09:59:59 +0800

    made the status bar rounded

--- MDPAndroidControl/app/src/main/java/com/example/mdpandroidcontrol/MainActivity.java
+++ MDPAndroidControl/app/src/main/java/com/example/mdpandroidcontrol/MainActivity.java
@@ -797,11 +797,11 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             for (int x=0;x<mapView.getColumns();x++){
                 mapPos[y][x].setText("");//Clear Text
                 switch (currentMap[y][x]){
-                    case -1://Default Unexplored
+                    case 0://Default Unexplored
                         //ViewCompat.setBackgroundTintList(mapPos[y][x], ContextCompat.getColorStateList(this, android.R.color.darker_gray));
                         mapPos[y][x].setBackgroundColor(0x70000000);//Set the background color of the map to be white
                         break;
-                    case 0://Explored
+                    case -1://Explored
                         mapPos[y][x].setBackgroundColor(0x10000000);//Set the background color of the map to be white
                         break;
                     case 1://Obstacles
